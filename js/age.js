14ba00ce018802f6ad1490a3cdafb5c7c88b58ff
Mourad CHIBANE <chimou.rabane@gmail.com>
2013-01-27 02:03:35 +0100

    Update js/age.js

--- js/age.js
+++ js/age.js
@@ -5,7 +5,7 @@ var jour = dt.getDate();
 var annee = dt.getFullYear();	
 var x = ((jour - 13) + (30 * (mois - 6) + (365* (annee - 1986))))/ 365;
 var mnAge = Math.round(x);
-return mnAge;
+return mnAge - 1;
 }
 
 $(function(){
